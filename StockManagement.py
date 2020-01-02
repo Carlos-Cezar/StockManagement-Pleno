78f6e2b8426c0c949a770e28a358d5b785c97f68
Carlos-Cezar <57515308+Carlos-Cezar@users.noreply.github.com>
2020-01-02 15:23:04 -0300

    Adicionado leitor de codigo de barras

--- StockManagement.py
+++ StockManagement.py
@@ -9,11 +9,18 @@ tz = pytz.timezone('America/Sao_Paulo')
 
 ### ↓ Estoque ↓ ###
 
+
 with open('notes_disponiveis_dicionario', 'r') as f:
   notes_disponiveis_dicionario = json.load(f)
 
 with open('notes_evento_dicionario', 'r') as f:
   notes_evento_dicionario = json.load(f)
+
+
+#### testing ### 
+
+with open('codigo_de_barras', 'r') as f:
+  codigo_de_barras = json.load(f)
 
 
 ### ↑ Estoque ↑ ### 
@@ -80,70 +87,70 @@ def main():
  
 def retirar():
  print_notes_disponiveis()
- note_selecionado = input("\nDigite o id do notebook que você deseja retirar:")
- note_selecionado = note_selecionado.upper()
- if note_selecionado in notes_disponiveis_dicionario:
-   notes_disponiveis_dicionario.pop(note_selecionado)
-   evento = input("Nome do evento:")
-   data = input("Data de retorno:")
-   logs = open("logs.txt","a+")
-   logs.write("\nID: {} |Retirou o notebook: {} | para o evento: {} | às {} |".format(id, note_selecionado, evento, datetime.now(tz)))
-   logs.close()
-   notes_evento_dicionario[note_selecionado] = (evento, data)
-   with open("notes_disponiveis_dicionario", 'w') as f:
-     json.dump(notes_disponiveis_dicionario, f)
-   with open("notes_evento_dicionario", 'w') as f:
-     json.dump(notes_evento_dicionario, f)
-   print_notes_disponiveis()
-   print_notes_evento()
-   print("Notebook {} retirado com sucesso.".format(note_selecionado)
-   )
-   def confirmar_retirada():
-     confirmar = input('\nGostaria de retirar outro notebook? \n[1] Sim \n[2] Não \nPleno:')
-     if confirmar == "1":
-       retirar()
-     elif confirmar == "2":
-       print("Ok, você será redirecionado para o inicio do programa.")
-       main()
-     else:
-       print("Número da ação inválido.")
-       confirmar_retirada()
-   confirmar_retirada()
-  
+ codigo_escaneado = getpass("\nEscaneie o código de barras do notebook que você deseja retirar:")
+ if codigo_escaneado in codigo_de_barras:
+   note_selecionado = codigo_de_barras.get(codigo_escaneado)
+   if note_selecionado in notes_disponiveis_dicionario:
+     notes_disponiveis_dicionario.pop(note_selecionado)
+     evento = input("Nome do evento:")
+     data = input("Data de retorno:")
+     logs = open("logs.txt","a+")
+     logs.write("\nID: {} |Retirou o notebook: {} | para o evento: {} | às {} |".format(id, note_selecionado, evento, datetime.now(tz)))
+     logs.close()
+     notes_evento_dicionario[note_selecionado] = (evento, data)
+     with open("notes_disponiveis_dicionario", 'w') as f:
+       json.dump(notes_disponiveis_dicionario, f)
+     with open("notes_evento_dicionario", 'w') as f:
+       json.dump(notes_evento_dicionario, f)
+     print_notes_disponiveis()
+     print_notes_evento()
+     print("Notebook {} retirado com sucesso.".format(note_selecionado))
+     def confirmar_retirada():
+       confirmar = input('\nGostaria de retirar outro notebook? \n[1] Sim \n[2] Não \nPleno:')
+       if confirmar == "1":
+         retirar()
+       elif confirmar == "2":
+         print("Ok, você será redirecionado para o inicio do programa.")
+         main()
+       else:
+         print("Número da ação inválido.")
+     confirmar_retirada()
+     
  else:
-   print("ID do notebook selecionado é inválido.")
+   print("Código de barras escaneado inválido.")
    main()
  
 def retorno():
  print_notes_evento()
- note_selecionado = input("Digite o número do notebook que você deseja retornar:")
- note_selecionado = note_selecionado.upper()
- if note_selecionado in notes_evento_dicionario:
-   notes_evento_dicionario.pop(note_selecionado)
-   notes_disponiveis_dicionario[note_selecionado] = 'Na unidade.'
-   with open("notes_evento_dicionario", 'w') as f:
-     json.dump(notes_evento_dicionario, f)
-   with open("notes_disponiveis_dicionario", 'w') as f:
-     json.dump(notes_disponiveis_dicionario, f)
-   logs = open("logs.txt","a+")
-   logs.write("\nID: {} |Retornou o notebook: {} | às {} |".format(id, note_selecionado, datetime.now(tz)))
-   logs.close()
-   print_notes_disponiveis()
-   print_notes_evento()
-   print("Notebook {} retornado com sucesso.".format(note_selecionado))
-   def confirmar_retorno():
-     confirmar = input('\nGostaria de retornar outro notebook? \n[1] Sim \n[2] Não \nPleno:')
-     if confirmar == "1":
-       retorno()
-     elif confirmar == "2":
-       print("Ok, você será redirecionado para o inicio do programa.")
-       main()
-     else:
-       print("Número da ação inválido.")
-       confirmar_retorno()
-   confirmar_retorno()
+ codigo_escaneado = getpass("\nEscaneie o código de barras do notebook que você deseja retornar:")
+ if codigo_escaneado in codigo_de_barras:
+   note_selecionado = codigo_de_barras.get(codigo_escaneado)
+   if note_selecionado in notes_evento_dicionario:
+     notes_evento_dicionario.pop(note_selecionado)
+     notes_disponiveis_dicionario[note_selecionado] = 'Na unidade.'
+     with open("notes_evento_dicionario", 'w') as f:
+       json.dump(notes_evento_dicionario, f)
+     with open("notes_disponiveis_dicionario", 'w') as f:
+       json.dump(notes_disponiveis_dicionario, f)
+     logs = open("logs.txt","a+")
+     logs.write("\nID: {} |Retornou o notebook: {} | às {} |".format(id, note_selecionado, datetime.now(tz)))
+     logs.close()
+     print_notes_disponiveis()
+     print_notes_evento()
+     print("Notebook {} retornado com sucesso.".format(note_selecionado))
+     def confirmar_retorno():
+       confirmar = input('\nGostaria de retornar outro notebook? \n[1] Sim \n[2] Não \nPleno:')
+       if confirmar == "1":
+         retorno()
+       elif confirmar == "2":
+         print("Ok, você será redirecionado para o inicio do programa.")
+         main()
+       else:
+         print("Número da ação inválido.")
+         confirmar_retorno()
+     confirmar_retorno()
  else:
-   print("ID do notebook selecionado inválido.")
+   print("Código de barras escaneado inválido.")
    main()
  
 ### ↑ Funções ↑ ###
